fc5d9f2140f9032e2cef75853d68705fb9d945c1
Niyi <Niyi@omoniyis-mbp.fios-router.home>
2017-09-10 22:39:36 -0400

    Commit: Adding jgoodies libraries needed to run the application

--- CISCComputer/src/CISC/FrontPanel.java
+++ CISCComputer/src/CISC/FrontPanel.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
 
 public class FrontPanel extends JFrame {
 
@@ -32,11 +37,44 @@ public class FrontPanel extends JFrame {
 	 */
 	public FrontPanel() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblPC = new JLabel("PC");
+		contentPane.add(lblPC, "2, 2");
+		
+		JLabel lblMar = new JLabel("MAR");
+		contentPane.add(lblMar, "2, 4");
+		
+		JLabel lblMbr = new JLabel("MBR");
+		contentPane.add(lblMbr, "2, 6");
+		
+		JLabel lblCc = new JLabel("CC");
+		contentPane.add(lblCc, "2, 8");
+		
+		JLabel lblMfr = new JLabel("MFR");
+		contentPane.add(lblMfr, "2, 10");
+		
+		JLabel lblIr = new JLabel("IR");
+		contentPane.add(lblIr, "2, 12");
 	}
 
 }
