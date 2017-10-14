c98488ae0c7b08c592a4b79add8164ed6d4952b0
[Kevin_Zhu] <[318272797@qq.com]>
2017-10-14 13:43:55 -0400

    new

--- CISCComputer/src/CISC/Cache.java
+++ CISCComputer/src/CISC/Cache.java
@@ -1,92 +1,104 @@
 package CISC;
 
 public class Cache {
-	int[][][]	AM;//Associative Memory
+	//![line][row]
+	int[][][]	AM;//Associative Memory  0 is whether in Cash, 1 is which line in the group
 	int[][][][]	HSM;//High Speed Memory
 	int HSMHeight;
 	int[] HSMpointer;//[line] check the HSM whether full of instruction.
 	int[][] HSMcheck;//check the HSM whether is empty, 1 is no, 0 is empty.
+	int nline;
+	int nrow;
 	int nword;//how many words in a units.
-	public Cache(MainMemory Memory, int HSMHeight) {
+	int[][] Table;
+	int linen;
+	int rown;
+	int wordn;
+	int Length;
+	
+	public Cache(int Length, int[][] Table, int nline, int nrow, int nword, int HSMHeight) {
 		//[line,row,word,bits]
 		//the row of high speed memory is less than memory
-		this.HSM=new int[Memory.nline][HSMHeight][Memory.nword][Memory.Length+1];//16+1,the first one is record the order.
+		this.HSM=new int[nline][HSMHeight][nword][Length];//16+1,the first one is record the order.
 		//the row and line of Associative memory as same as Memory
 		//nword
-		this.nword=Memory.nword;
-		this.AM=new int[Memory.nline][Memory.nrow][2];
-		for(int i=0;i<Memory.nline;i++) {
-			for(int j=0;j<Memory.nrow;j++) {
-				this.AM[i][j][0]=0;
-			}
-		}
+		this.Table=Table;
+		this.nline=nline;
+		this.nrow=nrow;
+		this.nword=nword;
+		this.Length=Length;
+		this.HSMHeight=HSMHeight;
+		
+		this.linen=this.nline;
+		this.rown=this.nrow;
+		
+		this.AM=new int[this.nline][this.nrow][2];
 		
 		//initiate the HSMpointer
-		this.HSMpointer=new int[Memory.nline];
-		for(int i=0;i<Memory.nline;i++) {
+		this.HSMpointer=new int[this.nline];
+		for(int i=0;i<this.linen;i++) {
 			this.HSMpointer[i]=1;//start from 1,is 0 means no data
 		}
 		
 		//initiate the HSMcheck
-		this.HSMcheck=new int[Memory.nline][HSMHeight];
-		for(int i=0;i<Memory.nline;i++) {
-			for(int j=0;j<HSMHeight;j++) {
-				this.HSMcheck[i][j]=0;
-			}
-		}
+		this.HSMcheck=new int[this.nline][HSMHeight];
 	};
 	
-	public int[] Search(int line, int row, int word, MainMemory Memory) {
+
+	
+	public int[] Search(int line, int row, int word, int[][] Memory) {//search data from Memory
 		int flag=0;//judge whether can find in the
-		int HSMrow=0;
-		for(int i=0;i<this.HSMHeight;i++) {
-			if(this.AM[line][i][0]==1) {
-				flag=1;
-				HSMrow=this.AM[line][i][1];
-				break;
-			}
+		int HSMrow=0;//
+		if(this.AM[line][row][0]==1) {//Memory in the Cashe
+			flag=1;
+			HSMrow=this.AM[line][row][1];
 		}
 		if(flag==1) {
-			return this.HSM[line][HSMrow][word];
+			return this.HSM[line][HSMrow][word];//fetch data from HSM
 		}
 		else {
-			this.InsertAddress(line, row,Memory);
+			HSMrow=this.InsertAddress(line,row,word,Memory);
 			//insert a unit word into HSM
-			return this.Search(line, row, word, Memory);
+			return this.HSM[line][HSMrow][word];
 		}
 	};
 	
-	private void InsertAddress(int line, int row, MainMemory Memory) {
+	private int InsertAddress(int line, int row, int word,int[][] Memory) {
 		int flag=0;
+		int HSMrow=0;
 		for(int i=0;i<this.HSMHeight;i++) {
 			if(this.HSMcheck[line][i]==0) {
-				flag=1;
-				for(int j=0;j<this.nword;j++) {
-					for(int n=0;n<Memory.Length;n++) {
-						int[] temp=Memory.Output(Memory.nline*line+Memory.nrow*row);
-						this.HSM[line][i][j][n]=temp[n];
-						this.HSMcheck[line][i]=HSMpointer[line];//insert into check
-						this.HSMpointer[line]++;//set the pointer express the order
-					}
+				flag=1;//can insert
+				HSMrow=i;
+				for(int j=0;j<this.nword;j++) {//insert a block in cashe
+					int[] temp=Memory[this.nline*line*this.nrow+row*this.nword+j];
+					this.HSM[line][i][j]=temp;	
 				}
+				this.HSMcheck[line][i]=HSMpointer[line];//insert into check
+				this.HSMpointer[line]++;//set the pointer express the order
+				this.AM[line][row][0]=1;//means have inserted in cashe
+				this.AM[line][row][1]=i;
+				break;
 			}
 		}
 		if(flag==0) {//flag==0 means there no place to insert.
 			for(int i=0;i<this.HSMHeight;i++) {
 				if(this.HSMcheck[line][i]==1) {//the first one
+					HSMrow=i;
 					for(int j=0;j<this.nword;j++) {//insert
-						for(int n=0;n<Memory.Length;n++) {
-							int[] temp=Memory.Output(Memory.nline*line+Memory.nrow*row);
+						int[] temp=Memory[this.nline*line*this.nrow*this.nword+this.nrow*row*this.nword+j];
+						for(int n=0;n<this.Length;n++) {	
 							this.HSM[line][i][j][n]=temp[n];
-							this.HSMcheck[line][i]=HSMpointer[line];//insert into check
-							//because no place to insert so we dont need to point++
 						}
 					}
+					this.HSMcheck[line][i]=HSMpointer[line];//insert into check
+					//because no place to insert so we dont need to point++
 					for(int n=0;n<HSMHeight;n++) {//all HSMcheck--
 						this.HSMcheck[line][n]--;
 					}
 				}
 			}
 		}
+		return HSMrow;
 	}
 }
--- CISCComputer/src/CISC/MainMemory.java
+++ CISCComputer/src/CISC/MainMemory.java
@@ -1,16 +1,19 @@
 package CISC;
 
 public class MainMemory extends Register{
-	int nline;
+	int nline;//how many in each line
 	int nrow;
 	int nword;
 	int[][] Address_Table;
+	Cache cache;
+	
 	public MainMemory(int bits, int height, int nline, int nrow, int nword) {
 		super(bits, height);
 		this.nline=nline;
 		this.nrow=nrow;
 		this.nword=nword;
 		Initiate_table();
+		this.cache=new Cache(this.Length,this.Address_Table,this.nline,this.nrow,this.nword,4);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -27,15 +30,59 @@ public class MainMemory extends Register{
 	//create the table, divide the table to line and row.
 	public void Initiate_table() {
 		this.Address_Table=new int[this.nline][this.nrow];
-		this.Address_Table[0][0]=0;
-		for(int i=1;i<this.nline;i++) {
-			this.Address_Table[i][0]=this.Address_Table[i-1][0]+this.nrow;
-		}
+		int n=0;
 		for(int i=0;i<this.nline;i++) {
-			for(int j=1;j<this.nrow;j++) {
-				this.Address_Table[i][j]=this.Address_Table[i][j-1]+this.nword;
+			for(int j=0;j<this.nrow;j++) {
+				this.Address_Table[i][j]=n;
+				n=n+this.nword;
 			}
 		}
 	}
-
+	
+	
+	public int[] Output(int[] Address){//binary is address
+		this.Output=new int[this.Length];
+		int decAd;
+		decAd=Binary_to_dec(Address);
+		if(decAd < Height) {
+			this.Output = this.Separate(decAd);
+		}else {
+			//Return error code
+			return null;
+		}
+		return this.Output;
+	}
+	
+	public int[] Output(int Address) {
+		this.Output=new int[this.Length];
+		if(Address < Height) {
+			this.Output = this.Separate(Address);
+		}else {
+			//Return error code
+			return null;
+		}
+		return this.Output;
+	}
+	
+	public int[] Separate(int EA) {//
+		int line;
+		int row;
+		int word;
+		int[] error=new int[] {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+		for(int i=0;i<this.nline;i++) {
+			if(this.Address_Table[i][0]>EA) {
+				line=i-1;
+				i=i-1;
+				for(int j=0;j<this.nrow;j++) {
+					if(this.Address_Table[i][j]>EA) {
+						row=j-1;
+						j=j-1;
+						word=EA-(line*this.nline*this.nrow*this.nword+row*this.nword);//!?
+						return this.cache.Search(line, row, word, this.Memory);
+					}
+				}
+			}
+		}
+		return error;
+	}
 }
